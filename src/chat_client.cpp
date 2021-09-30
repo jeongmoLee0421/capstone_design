bed7c4a156a86106ecfcee14eda13010fcd6aacc
leejeongmo <mermaid3605@naver.com>
2021-09-30 02:46:35 +0000

    modify chat_client.cpp

--- src/chat_client.cpp
+++ src/chat_client.cpp
@@ -54,21 +54,7 @@ int main(int argc, char* argv[]) {
 }
 
 void SendMsg(SOCKET sock) {
-	const char* dummy_str = "[32] Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, "
-		"totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. "
-		"Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, "
-		"qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur,"
-		"adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. "
-		"Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi "
-		"consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, "
-		"vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio dignissimos ducimus, "
-		"qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati "
-		"cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. "
-		"Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque "
-		"nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. "
-		"Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint "
-		"et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias "
-		"consequatur aut perferendis doloribus asperiores repellat.";
+	const char* dummy_str = "dummy_str";
 
 	int i = 0;
 	int strLen = strlen(dummy_str);
@@ -77,8 +63,8 @@ void SendMsg(SOCKET sock) {
 		Sleep(10);
 		i++;
 
-		// 100번 전송하고 스레드 종료
-		if (i == 100)
+		// 1000번 전송하고 스레드 종료
+		if (i == 1000)
 			return;
 	}
 }
